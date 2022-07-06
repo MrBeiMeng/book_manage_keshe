0c5f6b87726a4f7d7582fdee8ce5e40ae9f01f86
阳光 <1192384722@qq.com>
2022-07-06 18:07:01 +0800

    引入mybatis-plus\swagger3\ 创建user 表，生成对应的controller\mapper\service

--- src/main/java/club/beimeng/book_manage_keshe/BookManageKesheApplication.java
+++ src/main/java/club/beimeng/book_manage_keshe/BookManageKesheApplication.java
@@ -1,13 +1,15 @@
 package club.beimeng.book_manage_keshe;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("club.beimeng.book_manage_keshe.mapper")
 public class BookManageKesheApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookManageKesheApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookManageKesheApplication.class, args);
+    }
 
 }
---
+++ src/main/java/club/beimeng/book_manage_keshe/controller/AuthController.java
@@ -0,0 +1,19 @@
+package club.beimeng.book_manage_keshe.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author 包龙
+ * @since 2022-07-06
+ */
+@RestController
+@RequestMapping("/book_manage_keshe/auth")
+public class AuthController {
+
+}

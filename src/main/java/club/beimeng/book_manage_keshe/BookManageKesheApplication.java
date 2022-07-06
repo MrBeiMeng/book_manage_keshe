dbfe32e0d07c7a28535504e843ac3437749a087d
阳光 <1192384722@qq.com>
2022-07-06 12:40:32 +0800

    Initial commit

---
+++ src/main/java/club/beimeng/book_manage_keshe/BookManageKesheApplication.java
@@ -0,0 +1,13 @@
+package club.beimeng.book_manage_keshe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManageKesheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManageKesheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/club/beimeng/book_manage_keshe/BookManageKesheApplicationTests.java
@@ -0,0 +1,13 @@
+package club.beimeng.book_manage_keshe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManageKesheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
